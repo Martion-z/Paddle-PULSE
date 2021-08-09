d97d8d56bd009cf5252876e62ff7a10a84306241
Martion-z <53423816+Martion-z@users.noreply.github.com>
2021-08-09 16:02:31 +0800

    Add files via upload

---
+++ bicubic.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+import paddle
+from paddle import nn
+from paddle.nn import functional as F
+import math
+import paddle.fluid as fluid
+
+class BicubicDownSample(nn.Layer):
+    def bicubic_kernel(self, x, a=-0.50):
+        """
+        This equation is exactly copied from the website below:
+        https://clouard.users.greyc.fr/Pantheon/experiments/rescaling/index-en.html#bicubic
+        """
+        abs_x = paddle.abs(x)
+        #print(x)
+        if abs_x <= 1.:
+            return (a + 2.) * paddle.pow(abs_x, 3.) - (a + 3.) * paddle.pow(abs_x, 2.) + 1
+        elif 1. < abs_x < 2.:
+            return a * paddle.pow(abs_x, 3) - 5. * a * paddle.pow(abs_x, 2.) + 8. * a * abs_x - 4. * a
+        else:
+            return paddle.to_tensor(0.0, dtype=paddle.float32)
+
+    def __init__(self, factor=4, cuda=True, padding='reflect'):
+        super().__init__()
+        self.factor = factor
+        size = factor * 4
+        tk=[]
+        for i in range(size):
+            
+            a=self.bicubic_kernel((i-paddle.floor(paddle.to_tensor(size / 2,dtype=paddle.float32))+0.5)/factor)
+            #print(a)
+            tk.append(a)
+              
+        k=paddle.to_tensor(tk,dtype=paddle.float32)
+        #print(k)
+        '''
+        k = paddle.to_tensor([self.bicubic_kernel((i - paddle.floor(paddle.to_tensor(size / 2,dtype=paddle.float32)) + 0.5) / factor)
+                          for i in range(size)], dtype=paddle.float32)
+        '''
+        k = k / paddle.sum(k)
+        # k = torch.einsum('i,j->ij', (k, k))
+        k1 = paddle.reshape(k, shape=(1, 1, size, 1))
+        self.k1 = paddle.concat([k1, k1, k1], 0)
+        k2 = paddle.reshape(k, shape=(1, 1, 1, size))
+        self.k2 = paddle.concat([k2, k2, k2], 0)
+        self.cuda = '.cuda' if cuda else ''
+        self.padding = padding
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x, nhwc=False, clip_round=False, byte_output=False):
+        # x = torch.from_numpy(x).type('torch.FloatTensor')
+        filter_height = self.factor * 4
+        filter_width = self.factor * 4
+        stride = self.factor
+
+        pad_along_height = max(filter_height - stride, 0)
+        pad_along_width = max(filter_width - stride, 0)
+        #filters1 = self.k1.type('paddle{}.FloatTensor'.format(self.cuda))
+        #filters2 = self.k2.type('paddle{}.FloatTensor'.format(self.cuda))
+        filters1 = self.k1
+        filters2 = self.k2
+        # compute actual padding values for each side
+        pad_top = pad_along_height // 2
+        pad_bottom = pad_along_height - pad_top
+        pad_left = pad_along_width // 2
+        pad_right = pad_along_width - pad_left
+        
+        # apply mirror padding
+        if nhwc:
+            x = paddle.transpose(paddle.transpose(
+                x, 2, 3), 1, 2)   # NHWC to NCHW
+
+        # downscaling performed by 1-d convolution
+        x = F.pad(x, [0, 0, pad_top, pad_bottom], self.padding)
+        
+        x = F.conv2d(x, weight=filters1, stride=(stride, 1), groups=3)
+        #print(filters1)
+        if clip_round:
+            x = paddle.clip(paddle.round(x), 0.0, 255.)
+
+        x = F.pad(x, [pad_left, pad_right, 0, 0], self.padding)
+        x = F.conv2d(x, weight=filters2, stride=(1, stride), groups=3)
+        if clip_round:
+            x = paddle.clip(paddle.round(x), 0.0, 255.)
+
+        if nhwc:
+            x = paddle.transpose(paddle.transpose(x, 1, 3), 1, 2)
+        if byte_output:
+            return x.type('paddle.ByteTensor'.format(self.cuda))
+        else:
+            return x
+
+
---
+++ loss.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+import numpy
+import paddle
+import paddle.nn as nn
+import math
+from bicubic import BicubicDownSample
+import paddle.fluid as fluid
+
+class LossBuilder(nn.Layer):
+    def __init__(self, ref_im, loss_str, eps):
+        super(LossBuilder, self).__init__()
+        assert ref_im.shape[2]==ref_im.shape[3]
+        im_size = ref_im.shape[2]
+        factor=1024//im_size
+        assert im_size*factor==1024
+        self.D = BicubicDownSample(factor=factor)
+        self.ref_im = ref_im
+        self.parsed_loss = [loss_term.split('*') for loss_term in loss_str.split('+')]
+        self.eps = eps
+        
+
+    # Takes a list of tensors, flattens them, and concatenates them into a vector
+    # Used to calculate euclidian distance between lists of tensors
+    def flatcat(self, l):
+        l = l if(isinstance(l, list)) else [l]
+        return paddle.concat([x.flatten() for x in l], 0)
+
+    def _loss_l2(self, gen_im_lr, ref_im, **kwargs):
+        out=(gen_im_lr - ref_im)
+        #print(out)
+        out=out.pow(2).mean((1, 2, 3))
+        out=paddle.clip(out,min=self.eps)
+        return (out.sum())
+
+    def _loss_l1(self, gen_im_lr, ref_im, **kwargs):
+        out=10*(gen_im_lr - ref_im).abs().mean((1, 2, 3))
+        #return 10*((gen_im_lr - ref_im).abs().mean((1, 2, 3)).clamp(min=self.eps).sum())
+        out=paddle.clip(out,min=self.eps)
+        return out.sum()
+
+    # Uses geodesic distance on sphere to sum pairwise distances of the 18 vectors
+    def _loss_geocross(self, latent, **kwargs):
+        if(latent.shape[1] == 1):
+            return 0
+        else:
+            X = paddle.reshape(latent,shape=(-1, 1, 18, 512))
+            Y = paddle.reshape(latent,shape=(-1, 18, 1, 512))
+            A = ((X-Y).pow(2).sum(-1)+1e-9).sqrt()
+            B = ((X+Y).pow(2).sum(-1)+1e-9).sqrt()
+            #print(A)
+            #print(B)
+            D = 2*paddle.atan(A/B)
+            D = ((D.pow(2)*512).mean((1, 2))/8.).sum()
+            return D
+
+    def forward(self, latent, gen_im):
+        var_dict = {'latent': latent,
+                    'gen_im_lr': self.D(gen_im),
+                    'ref_im': self.ref_im,
+                    }
+        loss = 0
+        loss_fun_dict = {
+            'L2': self._loss_l2,
+            'L1': self._loss_l1,
+            'GEOCROSS': self._loss_geocross,
+        }
+        losses = {}
+        #print(gen_im)
+        for weight, loss_type in self.parsed_loss:
+            tmp_loss = loss_fun_dict[loss_type](**var_dict)
+            losses[loss_type] = tmp_loss
+            loss += float(weight)*tmp_loss
+        return loss, losses
+
